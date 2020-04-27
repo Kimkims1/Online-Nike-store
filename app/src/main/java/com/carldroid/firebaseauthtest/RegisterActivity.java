0689372b793e4a5125d0ec420359472b850f3b9c
Kimkims1 <kimanthikevin76@gmail.com>
2020-04-27 14:51:23 +0300

    nike store

--- app/src/main/java/com/carldroid/firebaseauthtest/RegisterActivity.java
+++ app/src/main/java/com/carldroid/firebaseauthtest/RegisterActivity.java
@@ -3,7 +3,6 @@ package com.carldroid.firebaseauthtest;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
