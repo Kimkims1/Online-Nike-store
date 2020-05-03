f9535286295305e12a8da3c9bbdc591a4270d1f0
Kimkims1 <kimanthikevin76@gmail.com>
2020-05-03 21:44:10 +0300

    nike store

--- app/src/main/java/com/carldroid/firebaseauthtest/DashBoardActivity.java
+++ app/src/main/java/com/carldroid/firebaseauthtest/DashBoardActivity.java
@@ -2,10 +2,20 @@ package com.carldroid.firebaseauthtest;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class DashBoardActivity extends AppCompatActivity {
+
+    private TextView nameTv,proffesionTv,emailTv,tabsJobsTv,tabAppliedJobsTv;
+    private EditText searchJobEt;
+    private RecyclerView AllJobsRv;
+
+    private ImageButton checkBalBtn,logoutBtn,editProfileBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
